46b0453be5d06097bed43f082c408e3ad6207a87
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-25 13:56:53 +0300

    Burger Menu App v1.0.0
    ...

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,20 @@
 import React,{useRef} from "react";
 
+import { useOnClickOutside } from '../../hooks';
+
 export type Props = {
     open: boolean;
     setOpen: (v:boolean) => void;
 }
 
 const Menu = (props: Props) => {
+    
+    const node = useRef<HTMLDivElement>(null);
+    useOnClickOutside(node, () => props.setOpen(false));
+
     return(
-        <>
-        </>
+        <div ref={node}>
+        </div>
     )
 };
 
